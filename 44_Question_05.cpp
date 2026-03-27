4a80483cd1a4148b6ade58233c48e684d91b507f
Ashish Kumar <soorveer31@gmail.com>
2026-03-27 18:35:03 +0530

    Updating code 2

--- 44_Question_05.cpp
+++ 44_Question_05.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
+int output(int num){
+    for (int i = 1; i <= 30; i++)
+    {
+        int ans = pow(2,i);
+        if (ans==num)
+        {
+            return true;
+        }
+    }
+    return false;
+    
+}
 int main(){
     int num;
     cout<<"Enter the number: ";
     cin>>num;
 
-    cout<<"Hello World";
+    cout<<"The number you entered is power of 2: "<<output(num);
     return 0;
 }
